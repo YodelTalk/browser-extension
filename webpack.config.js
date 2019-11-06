d7fed5745a62dd40ab6c53040b8a069d096651f6
Mario Uher <uher.mario@gmail.com>
2019-11-06 22:53:45 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,29 @@
-const path = require('path')
 const fs = require('fs')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WriteJsonPlugin = require('write-json-webpack-plugin')
 
 module.exports = {
   mode: 'production',
-  // Entry files for our popup and background pages
   entry: {
     content_script: './src/content_script.js',
     options: './src/options.js',
     background: './src/background.js',
     helper: './src/helper.js'
   },
-  // Extension will be built into ./dist folder, which we can then load as unpacked extension in Chrome
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
   },
-  // Here we define loaders for different file types
   module: {
     rules: [
-      // We use Babel to transpile JSX
       {
         test: /\.js$/,
         include: [
           path.resolve(__dirname, './src')
         ],
         use: 'babel-loader'
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: [
-          'file-loader?limit=100000',
-          {
-            loader: 'img-loader',
-            options: {
-              enabled: true,
-              optipng: true
-            }
-          }
-        ]
       }
     ]
   },
